54398677f4c88ff5195e736dc3c3bfcfa304a1df
ChingYenShih <b02504036@ntu.edu.tw>
2020-03-31 21:47:14 +0000

    separate tensorboard function from main.py to a new file tb_writer.py

--- final/main.py
+++ final/main.py
@@ -1,14 +1,14 @@
-import torch
-from dataloader.dataloader import get_loader
 import argparse
-from tqdm import tqdm
-from training.train import train_val, EarlyStop
-from model.DeepLidar import deepLidar
-import torch.optim as optim
 import os
-from tensorboardX import SummaryWriter
-from training.utils import *
 
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+
+from dataloader.dataloader import get_loader
+from model.DeepLidar import deepLidar
+from tb_writer import TensorboardWriter
+from training.train import EarlyStop, train_val
 
 parser = argparse.ArgumentParser(description='Depth Completion')
 parser.add_argument('-b', '--batch_size', type=int, default=16, help='batch size')
@@ -25,49 +25,35 @@ STAGE = args.stage.upper()
 SAVED_MODEL_PATH = 'saved_model'
 
 
-
-def get_testing_img(writer):
-    loader = get_loader('val', shuffle=False, num_data=1, crop=False)
-    for rgb, lidar, mask, gt_depth, params, gt_surface_normal, gt_normal_mask in loader:
-        writer.add_image('RGB input', rgb[0] / 255.0, 1)
-        writer.add_image('lidar input', lidar[0], 1)
-        writer.add_image('GroundTruth depth', normal_to_0_1(gt_depth[0]), 1)
-        writer.add_image('GroundTruth surface normal', normal_to_0_1(gt_surface_normal[0]), 1)
-        return rgb.to(DEVICE), lidar.to(DEVICE), mask.to(DEVICE)
-
-
-def tensorboard_write(writer, epoch, train_losses, val_losses, predicted_dense, pred_surface_normal):
-    loss_type = ['loss', 'loss_d', 'loss_c', 'loss_n', 'loss_normal']
-
-    for i, t in enumerate(loss_type):
-        writer.add_scalar('train_{}'.format(t), train_losses[i], epoch)
-        writer.add_scalar('val_{}'.format(t), val_losses[i], epoch)
-    writer.add_image('predicted_dense', normal_to_0_1(predicted_dense[0]), epoch)
-    writer.add_image('pred_surface_normal', normal_to_0_1(pred_surface_normal[0]), epoch)
-
-
-
 def main_train(model, stage):
-    writer = SummaryWriter('runs/{}_{}'.format(stage, args.saved_model_name))
-
-    loader = {'train': get_loader('train', num_data=args.num_data), \
-              'val': get_loader('val', shuffle=False, num_data=1000)}
+    # setting tensorboard
+    tensorboard_path = 'runs/{}_{}'.format(stage, args.saved_model_name)
+    tb_writer = TensorboardWriter(tensorboard_path)
     
-    testing_rgb, testing_lidar, testing_mask = get_testing_img(writer)
+    # get one testing image, used to visualize result in each eopch 
+    testing_rgb, testing_lidar, testing_mask = tb_writer.get_testing_img()
+    testing_rgb, testing_lidar, testing_mask = testing_rgb.to(DEVICE), testing_lidar.to(DEVICE), testing_mask.to(DEVICE)
 
-
+    # setting early stop, if result doen't improve more than PATIENCE times, stop iteration
     early_stop = EarlyStop(patience=10, mode='min')
+
+    # get data loader
+    loader = {'train': get_loader('train', num_data=args.num_data), \
+              'val': get_loader('val', shuffle=False, num_data=1)}
+    
+
     for epoch in range(args.epoch):
         saved_model_path = os.path.join(SAVED_MODEL_PATH, "{}_{}_e{}".format(args.saved_model_name, stage, epoch+1))
         train_losses, val_losses = train_val(model, loader, epoch, DEVICE, stage)
 
+        # predict dense and surface normal using testing image and write them to tensorboard
         predicted_dense, pred_surface_normal = get_depth_and_normal(model, testing_rgb, testing_lidar, testing_mask)
-        tensorboard_write(writer, epoch, train_losses, val_losses, predicted_dense, pred_surface_normal)
+        tb_writer.tensorboard_write(epoch, train_losses, val_losses, predicted_dense, pred_surface_normal)
 
         if early_stop.stop(val_losses[0], model, epoch+1, saved_model_path):
             break
 
-    writer.close()
+    tb_writer.close()
 
 
 
@@ -81,8 +67,8 @@ def main():
         print('Loss of loaded model: {:.4f}'.format(dic['val_loss']))
 
     main_train(model, 'N')
-    main_train(model, 'D')
-    main_train(model, 'A')
+    #main_train(model, 'D')
+    #main_train(model, 'A')
 
 
 if __name__ == '__main__':
